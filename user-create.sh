d9c332c30d75cb5546d305b045f3d2c7924e7d7a
Welligton Alves - Linux4Life <welligtonalves.ti@gmail.com>
2023-10-31 22:08:01 -0400

    Update user-create.sh

--- user-create.sh
+++ user-create.sh
@@ -2,94 +2,82 @@
 ####################################################################
 #       Script criado por Welligton Alves Analista eulabs      #####
 ####################################################################
+
 clear
 
 if [[ $EUID -ne 0 ]]; then
-echo "Você deve executar o script como root"
+   echo "você deve executar o script como root"
    exit 1
 fi
 
-read -p "Forneca o nome de login: " nome
+read -p "forneça o nome de login: " nome
 
+# Obter o endereço IP privado
+endereco_privado=$(hostname -I)
+
+# Obter o endereço IP público
+endereco_publico=$(curl -s ifconfig.me)
+
+echo "verificando se o usuário existe..."
 sleep 3
 
-echo -e "Verificando se o usuario existe!"
-
-cut -d: -f1 /etc/passwd | grep -iw $nome
-
-if [ $? -eq 0 ]; then
-
-echo 'Processo de criancao de usuario cancelado: USUARIO EXISTENTE!'
-
+# Verifique se o usuário já existe
+if id "$nome" &>/dev/null; then
+   echo "processo de criação de usuário cancelado: usuário já existe!"
    exit 1
+fi
 
-else
-
-echo -e "Criando usuario"
-
+echo "criando usuário..."
 sleep 3
 
+# Gerar uma senha aleatória
 senha=$(date | md5sum | head -c8)
 
-useradd -m -d /home/$nome -p $(openssl passwd $senha) -c $nome -s /bin/bash $nome
+# Criar o usuário
+useradd -m -d "/home/$nome" -p "$(openssl passwd -1 $senha)" -c "$nome" -s /bin/bash "$nome"
 
+echo "criando diretório do usuário..."
 sleep 3
 
-echo -e "Criando diretorio do usuario"
+# Criar o diretório .ssh e o arquivo authorized_keys
+mkdir -p "/home/$nome/.ssh"
+touch "/home/$nome/.ssh/authorized_keys"
 
-mkdir -p /home/$nome/.ssh
-
-echo -e "Criando arquivo autorized_key"
-
+echo "tornando o usuário dono do diretório /home/$nome..."
 sleep 3
 
-touch /home/$nome/.ssh/authorized_keys
+chown -R "$nome:$nome" "/home/$nome"
 
-echo -e "Tornando usuario dono do diretorio /home/$nome"
-
+echo "definindo permissões 0700 no diretório .ssh e 0600 no arquivo authorized_keys..."
 sleep 3
 
-chown -R $nome:$nome /home/$nome
+chmod 0700 "/home/$nome/.ssh"
+chmod 0600 "/home/$nome/.ssh/authorized_keys"
 
-echo -e "Dando as permissoes 0777 no diretorio .ssh e 0600 no aquivo autorized_key"
-sleep 3
-
-chmod 0700 /home/$nome/.ssh
-chmod 0600 /home/$nome/.ssh/authorized_keys
-
-echo -e "Editando o arquivo authorized_keys, adicone a chave key do usuario. Pressione: <Enter> para continuar"
+echo "editando o arquivo authorized_keys, adicione a chave SSH do usuário e pressione <Enter> para continuar"
 read
-	sleep 3
-
-	vim /home/$nome/.ssh/authorized_keys
-
-echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
-
 sleep 3
 
-echo "USUARIO $nome CRIADO COM SUCESSO. SENHA DE ACESSO: $senha "
-sleep 3
-fi
+# Use o editor de texto padrão para editar o arquivo authorized_keys
+sudo -u "$nome" vim "/home/$nome/.ssh/authorized_keys"
 
-echo -e "Adicionando usuario ao grupo sudo"
-
+echo "arquivo editado com sucesso!!! continuando com o script..."
 sleep 3
 
-GRUPO=%sudo
+echo "adicionando usuário ao grupo sudo..."
+sleep 3
 
-cut -d: -f1 /etc/sudoers | grep -iw $GRUPO
-
-if [ $? -eq 0 ]; then
-
-  usermod -aG sudo $nome
-
+# Verifique se o grupo sudo ou wheel existe e adicione o usuário apropriado
+if grep -qE "^%sudo|^%wheel" /etc/sudoers; then
+  usermod -aG sudo "$nome"
 else
-
-  usermod -aG wheel $nome
-
+  echo "não foi possível encontrar o grupo sudo ou wheel no arquivo /etc/sudoers."
 fi
 
-echo -e "USUARIO $nome AGORA PERTENCE AO GRUPO SUDO!"
+echo "usuário $nome agora pertence ao grupo sudo!"
+echo "usuário $nome criado com sucesso. senha de acesso: $senha"
+echo "endereço IP privado: $endereco_privado"
+echo "endereço IP público: $endereco_publico"
 
 sleep 3
 
